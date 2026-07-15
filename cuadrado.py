9bb8640347f05e10f4dafbc9e7c036daf37db380
Flores De La Paz Lani Joe <laniflores513@gmail.com>
2026-07-15 17:03:22 -0600

    Delete cuadrado.py

--- cuadrado.py
+++
@@ -1,7 +0,0 @@
-lado = float(input("ingresa la medida del cuadrado"))
-
-perimetro = lado * 4
-area = lado**2
-
-print("el perimetro del cuadrado es: ", perimetro)
-print("el area del cuadrado es. ", area)
